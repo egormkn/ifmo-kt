41d9472c0a9b7289871dbfe8e3e9ead12ec45707
Egor Makarenko <egormkn@yandex.ru>
2016-03-04 17:01:29 +0300

    Update rational.cpp

--- task3/rational.cpp
+++ task3/rational.cpp
@@ -14,37 +14,37 @@ rational::rational(int num, int denom) {
 int rational::getNum() const {
 	return rational::n;
 }
-		
+	
 int rational::getDenom() const {
 	return rational::d;
 }
-		
-rational::rational operator +(rational::rational const &a) const {
+	
+rational operator +(rational::rational const &a) const {
 	int nok = rational::d * a.d / gcd(rational::d, a.d);
 	int num = rational::n * (nok / rational::d) + a.n * (nok / a.d), denom = nok;
 	simplify(num, denom);
 	return rational(num, denom);
 }
-		
-rational::rational operator -(rational::rational const &a) const {
+	
+rational operator -(rational::rational const &a) const {
 	int nok = d * a.d / gcd(d, a.d);
 	int num = n * (nok / d) - a.n * (nok / a.d), denom = nok;
 	simplify(num, denom);
 	return rational(num, denom);
 }
-		
-rational::rational operator *(rational::rational const &a) const {
+	
+rational operator *(rational::rational const &a) const {
 	int num = n * a.n, denom = d * a.d;
 	simplify(num, denom);
 	return rational(num, denom);
 }
-		
-rational::rational operator /(rational::rational const &a) const {
+	
+rational operator /(rational::rational const &a) const {
 	int num = n * a.d, denom = d * a.n;
 	simplify(num, denom);
 	return rational(num, denom);
 }
-		
+	
 ~rational() {
     // Bye bye
 }
@@ -54,11 +54,11 @@ static int rational::gcd (int a, int b) {
 	return b ? gcd (b, a % b) : a;
 }
 
-static void simplify(int &n, int &d){
+static void rational::simplify(int &n, int &d){
 	int g = gcd(n, d);
 	while(g != 1) {
 		n = n / g;
 		d = d / g;
-		g = gcd(n, d);
+		g = rational::gcd(n, d);
 	}
 }
