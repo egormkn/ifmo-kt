a56941d744b2bc8b18ee850cd1f631e00e6b5557
Egor Makarenko <egormkn@yandex.ru>
2016-04-01 00:25:47 +0300

    Initial version of task4 (C++)

--- task4/format.cpp
+++ task4/format.cpp
@@ -1,154 +1,94 @@
 #include <string>
 #include <stdexcept>
+#include <cctype>
+#include <climits>
+#include "format.h"
 
 using namespace std;
 
 string format(const string &fmt){
-	return fmt;
-}
+	printed = 0;
+    unknown_spec = "";
 
-template<typename First, typename... Rest> string format(const string &fmt, First value, Rest... args){
-    string temp = "";
+    string result = "";
     unsigned pos = 0;
-    bool spec_found = false, spec_parsed = false;
-    while(!spec_found){
-        for(; pos < fmt.length() && fmt[pos] != '%'; temp.push_back(fmt[pos++]));
+    while(pos < fmt.length()){
+        for(; pos < fmt.length() && fmt[pos] != '%'; result.push_back(fmt[pos++]));
         if(pos == fmt.length()){
-	    	printf("Too many arguments for format [-Wformat-extra-args]\n");
-            return temp;
-	    }
-    	if(pos == fmt.length() - 1){
-	    	printf("Spurious trailing '%%' in format [-Wformat=]\n");
-            return temp;
-	    }
-	    if(fmt[pos + 1] == '%'){
-            temp.push_back('%');
+            return result;
+        } else if(pos == fmt.length() - 1){
+            printf("Spurious trailing '%%' in format [-Wformat=]\n");
+            return result;
+        } else if(fmt[pos + 1] == '%'){
+            result.push_back('%');
             pos += 2;
-		} else {
-			pos++;
-            spec_found = true;
-		}
-	}
-    /// fmt[pos] == '%' и после что-то есть
-    
-    /* char flags = 0;
-    int width = 0;
-    int precision = -1;
-	char length[5] = "00000";
-	char sign = '\0';
-	char type = ' '; */
-    while(!spec_parsed){
-        if(pos == fmt.length()){
-            printf("Сonversion lacks type at end of format [-Wformat=]\n");
-            return temp;
-    	}
-    
-        switch(fmt[pos]){
-            case 'd':
-            case 'i':
-                temp.append("(int)");
-                spec_parsed = true;
-			    break;
-			case 'u':
-			    temp.append("(unsigned int)");
-                spec_parsed = true;
-			    break;
-            case 'o':
-                temp.append("(unsigned octal)");
-                spec_parsed = true;
-			    break;
-            case 'x':
-                temp.append("(unsigned hex)");
-                spec_parsed = true;
-			    break;
-            case 'X':
-                temp.append("(UNSIGNED HEX)");
-                spec_parsed = true;
-			    break;
-            case 'f':
-                temp.append("(decimal float)");
-                spec_parsed = true;
-			    break;
-            case 'F':
-                temp.append("(DECIMAL FLOAT)");
-                spec_parsed = true;
-			    break;
-            case 'e':
-                temp.append("(scientific)");
-                spec_parsed = true;
-			    break;
-            case 'E':
-                temp.append("(SCIENTIFIC)");
-                spec_parsed = true;
-			    break;
-            case 'g':
-                temp.append("(%e or %f)");
-                spec_parsed = true;
-			    break;
-            case 'G':
-                temp.append("(%E or %F)");
-                spec_parsed = true;
-			    break;
-            case 'a':
-                temp.append("(hex float)");
-                spec_parsed = true;
-			    break;
-            case 'A':
-                temp.append("(HEX FLOAT)");
-                spec_parsed = true;
-			    break;
-            case 'c':
-                temp.append("(char)");
-                spec_parsed = true;
-			    break;
-            case 's':
-                temp.append("(string)");
-                spec_parsed = true;
-			    break;
-            case 'p':
-                temp.append("(pointer)");
-                spec_parsed = true;
-			    break;
-            case 'n':
-                spec_parsed = true;
-			    break;
-            /// Length
-            case 'h':
-            case 'l':
-            case 'j':
-            case 'z':
-            case 't':
-            case 'L':
-            /// Precision
-            case '.':
-            /// Width
-            case '*':
-            /// Flags
-            case '-':
-                // Left-justify within the given field width; Right justification is the default (see width sub-specifier).
-                break;
-            case '+':
-
-            case ' ':
-
-            case '#':
-                break;
-            case '0':
-                break;
-     	}
-     	pos++;
-	}
-    
-    return temp + format(fmt.substr(pos, fmt.length()), args...);
+        } else {
+            pos++;
+            printf("Need more arguments or unknown format\n");
+        }
+    }
+    return result;
 }
-
 
 int main(){
-	char zz[] = "foo%% test %% %%%% %d %f";
-    printf("%s\n", zz);
+    /*
+    long g = INT_MAX;
+    printf("%s\n", format("Format:   %-5.20lx %.10d", g, 10).c_str());
+    printf("Original: %-5.20lx %.10d\n", g, 10);
 
-	
-    printf(format(zz, 10, 0.5).c_str());
-	return 0;
+
+    int val = 0;
+    printf("%s\n", format("blah %n blah", &val).c_str());
+    printf("%s\n", format("val = %d\n", val).c_str());
+
+    printf("%s\n", format("%-*.*wd test", 10).c_str());
+
+
+
+    printf("Need: %");
+    printf("\n");
+    printf("Got:  %s\n", format("%").c_str());
+    
+    printf("NEED: %%");
+    printf("\n");
+    printf("GOT:  %s\n", format("%%").c_str());
+    
+    printf("Need: %%%");
+    printf("\n");
+    printf("Got:  %s\n", format("%%%").c_str());
+    
+    printf("NEED: %%%%");
+    printf("\n");
+    printf("GOT:  %s\n", format("%%%%").c_str());
+    
+    printf("NEED: test");
+    printf("\n");
+    printf("GOT:  %s\n", format("test").c_str());
+
+    printf("Need: %test");
+    printf("\n");
+    printf("Got:  %s\n", format("%test").c_str());
+    
+    printf("Need: %d");
+    printf("\n");
+    printf("Got:  %s\n", format("%d").c_str());
+
+    char zz[] = "foo%% test %% %%%% %i";
+    printf("Need: ");
+    printf(zz);
+    printf("\n");
+    printf("Got:  %s\n", format(zz).c_str());
+*/
+
+    printf("%s\n", format("Characters: %c %c \n", 'a', 65).c_str());
+    printf("%s\n", format("Decimals: %d %ld\n", 1977, 650000L).c_str());
+    printf("%s\n", format("Preceding with blanks: %10d \n", 1977).c_str());
+    printf("%s\n", format("Preceding with zeros: %010d \n", 1977).c_str());
+    printf("%s\n", format("Some different radices: %d %x %o %#x %#o \n", 100, 100, 100, 100, 100).c_str());
+    printf("%s\n", format("floats: %4.2f %+.0e %E \n", 3.1416, 3.1416, 3.1416).c_str());
+    printf("%s\n", format("Width trick: %*d \n", 5, 10).c_str());
+    printf("%s\n", format("%s \n", "A string").c_str());
+
+    
+    return 0;
 }
- 
