3a7eda9343ce792d04802ccfb36d3b816b2a2850
Egor Makarenko <egormkn@yandex.ru>
2016-04-28 19:58:03 +0300

    +

--- course1/algorithms/graphs/N_fire.cpp
+++ course1/algorithms/graphs/N_fire.cpp
@@ -18,9 +18,9 @@ int counter = 0;
  
 void dfs1 (int v) {
 	used[v] = true;
-	for (size_t i = 0; i < g[v].size(); ++i){
-		if (!used[g[v][i]]){
-			dfs1(g[v][i]);
+	for (size_t i = 0; i < gr[v].size(); ++i){
+		if (!used[gr[v][i]]){
+			dfs1(gr[v][i]);
 		}
 	}
 	order.push_back(v);
@@ -38,7 +38,6 @@ void dfs2 (int v) {
 
 int main(int argc, char **argv) {
     fscanf(fin, "%d %d", &n, &m);
-    int edgecount = 0;
     answer.assign(n, 0);
     
     for(int i = 0, v1, v2; i < m; i++) {
@@ -60,25 +59,13 @@ int main(int argc, char **argv) {
 			dfs2(v);
 			if(component.size() > 0){
                 counter++;
-                for(size_t j = 0; j < component.size(); j++){
-                    answer[component[j]] = counter;
-			    }
 			}
 			component.clear();
 		}
 	}
 
 
-	for (int i = 0; i < n; ++i) {
-        for(size_t j = 0; j < g[i].size(); j++){
-			if(answer[i] != answer[g[i][j]] && edges[answer[i]].find(answer[g[i][j]]) == edges[answer[i]].end()){
-				edges[answer[i]].emplace(answer[g[i][j]]);
-				edgecount++;
-			}
-		}
-	}
-
-	fprintf(fout, "%d %d\n", counter, edgecount);
+	fprintf(fout, "%d\n", counter);
     
     fclose(fin);
     fclose(fout);
